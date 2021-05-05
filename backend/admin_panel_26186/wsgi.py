cba8358540e407081f24089ff20289493bec4b3e
Crowdbotics <team@crowdbotics.com>
2021-05-05 19:36:46 +0000

    Update models

--- backend/admin_panel_26186/wsgi.py
+++ backend/admin_panel_26186/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'admin_panel_26186.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "admin_panel_26186.settings")
 
 application = get_wsgi_application()
